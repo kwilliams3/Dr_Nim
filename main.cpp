0770ca57bd64760d9655022516da454c3dfbe8d0
Kyle Williams <kwill036@plattsburgh.edu>
2017-09-26 00:09:26 -0400

    Create drNimsTurn function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 
 void playersTurn(int* numberOfMarbles) {
 
@@ -25,30 +27,37 @@ void playersTurn(int* numberOfMarbles) {
 }
 
 void drNimsTurn(int* numberOfMarbles) {
+    std::cout << "Dr. Nim is thinking about his move . . ." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    // Decrement the appropriate number of marbles
+    int marblesToTake = *numberOfMarbles % 4;
+    *numberOfMarbles -= marblesToTake;
+    std::cout << "Dr. Nim has decided to take " << marblesToTake << " marble(s) - leaving you with "
+              << *numberOfMarbles << " marble(s)." << std::endl << std::endl;
 }
 
 int main() {
-    using namespace std;
-    int numberOfMarbles = 15;
+    int numberOfMarbles = 12;
     int* numberOfMarblesPtr = &numberOfMarbles;
-    cout << endl
-         << "------------------------------------------------------------------------" << endl
-         << "Instructions:" << endl
-         << endl
-         << "This game is a test of your wit! You'll be going head to head - mind to" << endl
-         << "mind, against the famous Dr. Nim! The rules are simple. There are 15" << endl
-         << "marbles at the beginning of each game, and Dr. Nim is always courteous" << endl
-         << "enough to let you go first. Each turn you may choose to take 1, 2, or" << endl
-         << "even 3 marbles. Once you make your choice, it's Dr. Nim's turn. He too" << endl
-         << "is limited to taking only 1, 2, or 3 marbles each turn. You're goal is" << endl
-         << "to be the one who takes the very last marble. In doing so, you will" << endl
-         << "render yourself victorious against Dr. Nim - proving once and for all" << endl
-         << "that you are the true mastermind! Good luck!" << endl
-         << "------------------------------------------------------------------------" << endl;
+    std::cout << "\n"
+         << "------------------------------------------------------------------------\n"
+         << "Instructions:\n"
+         << "\n"
+         << "This game is a test of your wit! You'll be going head to head - mind to\n"
+         << "mind, against the famous Dr. Nim! The rules are simple. There are 12\n"
+         << "marbles at the beginning of each game, and Dr. Nim is always courteous\n"
+         << "enough to let you go first. Each turn you may choose to take 1, 2, or\n"
+         << "even 3 marbles. Once you make your choice, it's Dr. Nim's turn. He too\n"
+         << "is limited to taking only 1, 2, or 3 marbles each turn. You're goal is\n"
+         << "to be the one who takes the very last marble. In doing so, you will\n"
+         << "render yourself victorious against Dr. Nim - proving once and for all\n"
+         << "that you are the true mastermind! Good luck!\n"
+         << "------------------------------------------------------------------------\n\n";
     while (numberOfMarbles != 0) {
         playersTurn(numberOfMarblesPtr);
         drNimsTurn(numberOfMarblesPtr);
     }
-    std::cout << "Alas! Dr. Nim has claimed victory!";
+    std::cout << "Alas! Dr. Nim has claimed victory!" << std::endl;
     return 0;
 }
